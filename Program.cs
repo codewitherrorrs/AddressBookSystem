cc1d0fb508556685ae541325ee6f61cf18dac46c
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-17 14:19:13 +0530

    Add : Program.cs File

--- Program.cs
+++ Program.cs
@@ -4,17 +4,10 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Add Personal Details : firstname, lastname, email, address, city, state, pincode, phone");
-            string FirstName = Console.ReadLine();
-            string LastName = Console.ReadLine(); 
-            string Email = Console.ReadLine(); 
-            string Address = Console.ReadLine();
-            string City = Console.ReadLine();
-            string State = Console.ReadLine();
-            string Pincode = Console.ReadLine();
-            string Phone = Console.ReadLine();
-          
-        CreateContact c1 = new CreateContact(FirstName, LastName, Email, Address, City, State, Pincode, Phone);
+            CreatContact c1 = new CreatContact();
+            String[] names = {"Sagar","Krishna","Pankaj","Dharani"};
+            c1.change(names);
         }
+    
     }
 }
