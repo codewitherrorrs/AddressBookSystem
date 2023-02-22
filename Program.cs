afe37801fae572cf117c45ab382e4e72ce6e7837
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-22 12:58:09 +0530

    Add : Program.cs File

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            CreatContact c1 = new CreatContact();
-            String[] names = {"Sagar","Krishna","Pankaj","Dharani"};
-            c1.change(names);
+            //CreatContact c1 = new CreatContact();
+            //String[] names = { "Sagar", "Krishna", "Pankaj", "Dharani" };
+            //c1.change(names);
+
+            //CreateContactBuiltArray createContactBuiltArray = new CreateContactBuiltArray();
+            //createContactBuiltArray.addContact("sagar","shahu","nag","sss","sss","15544555","444454","sss@gmail.com");
+            //createContactBuiltArray.run();
+
+            AddressBookMain.AddContact();
+            //AddressBookMain.Display();
+
         }
-    
+
     }
 }
