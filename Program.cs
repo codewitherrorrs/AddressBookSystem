2a2be88f9d3059b5eb303fdda62190e422b5051e
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-06 12:46:50 +0530

    Ability to search Person in a city or state Across in Address Book

--- Program.cs
+++ Program.cs
@@ -6,11 +6,14 @@
         {
             List<Person> people = new List<Person>();
             people.Add(new Person { firstName = "Sagar",lastName = "Shahu",city = "Nagpur",state = "Maharashtra", zipcode = "440023",country = "India",phone = "55455665",email = "shahu@gmail.com" });
+            people.Add(new Person { firstName = "shahu", lastName = "Shahu", city = "Nagpur", state = "Maharashtra", zipcode = "440023", country = "India", phone = "5545556", email = "shu@gmail.com" });
+            people.Add(new Person { firstName = "vicky", lastName = "Shahu", city = "Nagpur", state = "Maharashtra", zipcode = "440023", country = "India", phone = "55455665", email = "shahu@gmail.com" });
+
 
             Person newEntry1 = new Person { firstName = "Sagar", lastName = "Shahu", city = "Nagpur", state = "Maharashtra", zipcode = "440023", country = "India", phone = "55455665", email = "shahu@gmail.com" };
-            if (people.Any(e => e.firstName == newEntry1.firstName))
+            if (people.Any(e => e.city == newEntry1.city || e.state == newEntry1.state))
             {
-                Console.WriteLine("Duplicate entry: {0}", newEntry1.firstName);
+                Console.WriteLine("Multiple Person in same state and same city" + " Name : "+newEntry1.firstName + ", State : "+newEntry1.state + ", City : "+newEntry1.city);
             }
             else
             {
